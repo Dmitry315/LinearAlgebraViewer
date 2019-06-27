eef1f0c712206278f9450bd082cabb03d7541051
Dmitry315 <dmitry.melihov2018@yandex.ru>
2019-06-27 13:34:05 +0300

    add decorators to check errors

--- gui.py
+++ gui.py
@@ -19,6 +19,31 @@ def update_information(window):
                 str(round(transformed_grid.vectors[transformed_grid.prev_vector[1]].coords[1], 3)))
 
 
+# decorator for checking exceptions in METHODS IN PYQT WINDOWS
+def checking_exceptions(method):
+    def wrapped(self):
+        try:
+            self.ErrorLog.setText('')
+            return method(self)
+        except TooManyVectors as err:
+            self.ErrorLog.setText('Too many vectors')
+        except Exception as err:
+            self.ErrorLog.setText('Error')
+
+    return wrapped
+
+
+# decorator for METHODS IN PYQT WINDOWS
+def check_is_vector_grabbed(method):
+    def wrapped(self):
+        try:
+            self.ErrorLog.setText('')
+            return method(self)
+        except Exception as err:
+            self.ErrorLog.setText('Vector not found. Choose vector.')
+    return wrapped
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -36,75 +61,56 @@ class MainWindow(QMainWindow):
         self.ChangeVectorRelativeCords.clicked.connect(self.change_vector_relative_cords)
 
     # get matrix and apply transformation
+    @checking_exceptions
     def apply(self):
-        try:
-            matrix = np.array([[float(self.matrix00.text()), float(self.matrix01.text())],
-                               [float(self.matrix10.text()), float(self.matrix11.text())]])
-            for i in range(2):
-                transformed_grid.basis_vectors[i].transform(matrix)
-            transformed_grid.transform()
-            update_information(self)
-            self.ErrorLog.setText('')
-        except Exception as err:
-            self.ErrorLog.setText('Error')
+        matrix = np.array([[float(self.matrix00.text()), float(self.matrix01.text())],
+                           [float(self.matrix10.text()), float(self.matrix11.text())]])
+        for i in range(2):
+            transformed_grid.basis_vectors[i].transform(matrix)
+        transformed_grid.transform()
+        update_information(self)
 
     # use grid coords
+    @checking_exceptions
     def add_vector(self):
-        try:
-            coords = [float(self.Vector_x.text()), float(self.Vector_y.text())]
-            transformed_grid.add_vector(coords)
-            transformed_grid.transform()
-            transformed_grid.prev_vector = ('common', -1)
-            update_information(self)
-        except TooManyVectors as err:
-            self.ErrorLog.setText('Too many vectors')
-        except Exception as err:
-            self.ErrorLog.setText('Error')
+        coords = [float(self.Vector_x.text()), float(self.Vector_y.text())]
+        transformed_grid.add_vector(coords)
+        transformed_grid.transform()
+        transformed_grid.prev_vector = ('common', -1)
+        update_information(self)
 
     # use basis vectors
+    @checking_exceptions
     def add_vector_rel(self):
-        try:
-            coords = [float(self.Vector_x_rel.text()), float(self.Vector_y_rel.text())]
-            transformed_grid.add_vector_rel(coords)
-            transformed_grid.transform()
-            update_information(self)
-        except TooManyVectors as err:
-            self.ErrorLog.setText('Too many vectors')
-        except Exception as err:
-            self.ErrorLog.setText('Error')
+        coords = [float(self.Vector_x_rel.text()), float(self.Vector_y_rel.text())]
+        transformed_grid.add_vector_rel(coords)
+        transformed_grid.transform()
+        update_information(self)
 
     # use grid coords
+    @check_is_vector_grabbed
     def change_vector_real_cords(self):
-        try:
-            coords = [float(self.Vector_x.text()), float(self.Vector_y.text())]
-            transformed_grid.vectors[transformed_grid.prev_vector[1]].transformed = coords
-            transformed_grid.vectors[transformed_grid.prev_vector[1]].coords = np.linalg.inv(
-                transformed_grid.get_matrix()).dot(
-                transformed_grid.vectors[transformed_grid.prev_vector[1]].transformed)
-            update_information(self)
-        except Exception as err:
-            self.ErrorLog.setText('Vector not found. Choose vector.')
+        coords = [float(self.Vector_x.text()), float(self.Vector_y.text())]
+        transformed_grid.vectors[transformed_grid.prev_vector[1]].transformed = coords
+        transformed_grid.vectors[transformed_grid.prev_vector[1]].coords = np.linalg.inv(
+            transformed_grid.get_matrix()).dot(
+            transformed_grid.vectors[transformed_grid.prev_vector[1]].transformed)
+        update_information(self)
 
     # use basis vectors
+    @check_is_vector_grabbed
     def change_vector_relative_cords(self):
-        try:
-            coords = [float(self.Vector_x_rel.text()), float(self.Vector_y_rel.text())]
-            transformed_grid.vectors[transformed_grid.prev_vector[1]].coords = coords
-            transformed_grid.transform()
-            update_information(self)
-        except Exception as err:
-            self.ErrorLog.setText('Vector not found. Choose vector.')
+        coords = [float(self.Vector_x_rel.text()), float(self.Vector_y_rel.text())]
+        transformed_grid.vectors[transformed_grid.prev_vector[1]].coords = coords
+        transformed_grid.transform()
+        update_information(self)
 
     # unapply all transformations
+    @checking_exceptions
     def reset(self):
-        try:
-            # identity matrix
-            matrix = np.array([[1, 0], [0, 1]])
-            for i in range(2):
-                transformed_grid.basis_vectors[i].transform(matrix)
-            transformed_grid.transform()
-            self.ErrorLog.setText('')
-            update_information(self)
-        except Exception as err:
-            self.ErrorLog.setText('Error')
-            raise err
+        # identity matrix
+        matrix = np.array([[1, 0], [0, 1]])
+        for i in range(2):
+            transformed_grid.basis_vectors[i].transform(matrix)
+        transformed_grid.transform()
+        update_information(self)
